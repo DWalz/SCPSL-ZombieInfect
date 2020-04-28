32f153f3e5b8d35bedb08c3e527349f192b3950c
DWalz <d.walz@zoho.com>
2020-04-28 18:41:45 +0200

    Add plugin

---
+++ SCPSL-ZombieInfect/Plugin.cs
@@ -0,0 +1,26 @@
+﻿using EXILED;
+
+namespace SCPSL_ZombieInfect
+{
+    public class ZombieInfect: Plugin
+    {
+        private string name = "ZombieInfect";
+        
+        public override void OnEnable()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override void OnDisable()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override void OnReload()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override string getName { get => name; }
+    }
+}
